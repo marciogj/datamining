88f4a3021bdbd1710b117f22d4e5be9206286693
Marcio.Jasinski <marcio.jasinski@senior.com.br>
2015-12-01 14:52:35 -0200

    Fixed issue on seeds classification

--- cluster/src/br/udesc/dcc/bdes/datamining/cluster/density/EsterDBScan.java
+++ cluster/src/br/udesc/dcc/bdes/datamining/cluster/density/EsterDBScan.java
@@ -5,35 +5,35 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.function.BiFunction;
 
 public class EsterDBScan<T> {
 	private static final Integer NOISE = 0;
-	//private static final Integer BORDER = 1;
-	//private static final Integer CORE = 2;
-	private static final Integer UNCLASSIFIED = null;
+	private static final Integer UNCLASSIFIED = -1;
 	
-	private Map<T, Integer> classified = new HashMap<>();
-	
-	public DBScanResult<T> evaluate(Collection<T> data, double eps, int minPts, BiFunction<T, T, Double> distanceFn) {
-
-		// SetOfPoints is UNCLASSIFIED
-		
+	public Map<T, Integer> dbscan(Collection<T> data, double eps, int minPts, BiFunction<T, T, Double> distanceFn) {
+		Map<T, Integer> classificationMap = new HashMap<>();
 		int clusterId = nextId(NOISE);
 		
 		for (T point : data) {
-			if ( !classified.containsKey(point) ) {
-				boolean isClusterExpanded = expandCluster(data, point, clusterId, eps, minPts, distanceFn);
+			if ( !classificationMap.containsKey(point) ) {
+				boolean isClusterExpanded = expandCluster(data, point, clusterId, eps, minPts, classificationMap, distanceFn);
 				if (isClusterExpanded) {
 					clusterId = nextId(clusterId);
 				}
 			}
 		}
+		return classificationMap;
+	}
+	
+	public DBScanResult<T> evaluate(Collection<T> data, double eps, int minPts, BiFunction<T, T, Double> distanceFn) {
+		Map<T, Integer> classificationMap = dbscan(data, eps, minPts, distanceFn);
 		
 		DBScanResult<T> result = new DBScanResult<>();
 		Map<Integer, Cluster<T>> clusters = new HashMap<>();
-		for(T point : classified.keySet()) {
-			Integer id = classified.get(point);
+		for(T point : classificationMap.keySet()) {
+			Integer id = classificationMap.get(point);
 			if (id == NOISE ) {
 				result.addNoise(point);
 			} else {
@@ -45,10 +45,7 @@ public class EsterDBScan<T> {
 				cluster.add(point);
 			}
 		}
-		
 		clusters.values().forEach( c -> result.addCluster(c));
-		
-		
 		return result;	
 	}
 	
@@ -72,27 +69,26 @@ public class EsterDBScan<T> {
 		return neighbors;
 	}
 
-	private boolean expandCluster(Collection<T> data, T point, Integer clusterId, double eps, int minPts, BiFunction<T, T, Double> distanceFn) {
-		
+	private boolean expandCluster(Collection<T> data, T point, Integer clusterId, double eps, int minPts, Map<T, Integer> classificationMap, BiFunction<T, T, Double> distanceFn) {		
 		List<T> seeds = regionQuery(data, point, eps, distanceFn); //regionQuery(point, eps)
-				
 		if (seeds.size() < minPts) {
-			classified.put(point, NOISE);
+			classificationMap.put(point, NOISE);
 			return false;
 		} else { // all points in seeds are density-reachable from Point
-			classified.put(point, clusterId);
+			//classificationMap.put(point, clusterId); //TODO: SEEDS
+			seeds.forEach( p -> classificationMap.put(p, clusterId));
 			seeds.remove(point);
 			while(!seeds.isEmpty()) {
 				T currentPoint = seeds.get(0); //first
 				List<T> result = regionQuery(data, currentPoint, eps, distanceFn);
 				if (result.size() >= minPts) {
 					for(T resultP : result) {
-						Integer classification = classified.get(resultP);
+						int classification = Optional.ofNullable(classificationMap.get(resultP)).orElse(UNCLASSIFIED);
 						if (classification == UNCLASSIFIED || classification == NOISE) {
 							if (classification == UNCLASSIFIED) {
 								seeds.add(resultP);
 							}
-							classified.put(resultP, clusterId);
+							classificationMap.put(resultP, clusterId);
 						} // UNCLASSIFIED or NOISE
 					} //END FOR
 				} // END IF result.size >= MinPts
